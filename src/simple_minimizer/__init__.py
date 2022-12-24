a8777d9b3bbd2c0b38264f79c913735d915ba2d0
Tom Radcliffe <tj@tjradcliffe.com>
2022-12-24 15:57:07 -0800

    Added logistic constraints (not integarated into minimizer yet)

--- src/simple_minimizer/__init__.py
+++ src/simple_minimizer/__init__.py
@@ -1,3 +1,4 @@
 
 from .minimizer import SimpleMinimizer
 from .vertex import CopyVertex, EmptyVertex, SimpleVertex
+from .logistic_constraints import LowerLogisticConstraint, UpperLogisticConstraint
